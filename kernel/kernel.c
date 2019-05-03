11081484e48dd21500a3ce44a4cac9dd224a6b51
Arash Fotouhi <arash@arashfotouhi.com>
2019-05-03 14:16:43 -0700

    Reverting to pre-character write debug.
    At this point I can properly write a character to screen using print_char

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,10 +1,7 @@
 #include "../drivers/screen.h"
 
 void main () {
-	//char* Video_memory = (char*) 0xb8000;
-	//*Video_memory = 'X';
-	//clear_screen();
+	clear_screen();
 	print_char('A', 0, 0, WHITE_ON_BLACK);
-	//print_char('d', -1, -1, WHITE_ON_BLACK);
-	//print_char('e', -1, -1, WHITE_ON_BLACK);
+	print_char('d', 1, 2, WHITE_ON_BLACK);
 }
