ca35b2cc5cda8701d611fb00df396f4701e7b0aa
Arash Fotouhi <arash@arashfotouhi.com>
2019-05-06 21:17:33 -0700

    Reverting kernel.c back to pre-scroll test.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -3,11 +3,6 @@
 void main (void) 
 {
 	clear_screen();
-	int i;
-	for (i=0; i<10000; i++) {
-		//print("MiniOS kernel successfully loaded.\n");
-		//print("Welcome!");
-		print_uint(i);
-		print(" ");
-	}
+	print("MiniOS kernel successfully loaded.\n");
+	print("Welcome!");
 }
