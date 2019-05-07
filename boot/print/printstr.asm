3d3bde05105e7ec995edf407dd7d2aad8129a338
Arash Fotouhi <arash@arashfotouhi.com>
2019-05-06 18:59:35 -0700

    Adding comments.

--- boot/print/printstr.asm
+++ boot/print/printstr.asm
@@ -1,13 +1,24 @@
+; Simple routine for printing a string on the display
 printstr:
-	pusha
+	pusha ; First push everything on the stack for later recovery
+
+	; We'll be using interrupt 0x10 to print to screen. The interrupt 
+	; "parameter" comes through the AX register. 0xE is the value that
+	; says write character in TTY mode. See BIOS interrupt table for
+	; details.
 	mov ah, 0x0e
 loop_printstr:
+	; We use bx for passing arguments to this routine. Here since the
+	; argument that is bassed is the address of the start of the
+	; string, we use [] to get the content of the address. The lower
+	; bits of the AX register hold the character to be displayed.
 	mov al, [bx]
+	; Call the interrupt to actually write the character to display.
 	int 0x10
-	add bx, 0x1
-	mov cx, [bx]
-	cmp cl, 0
-	jne loop_printstr
+	add bx, 0x1 ; Go to the next character
+	mov cx, [bx] ; Place character in CX for checking end of character
+	cmp cl, 0 ; Is this the end of character?
+	jne loop_printstr ; If not end of character print next character
 
-	popa
+	popa ; Restore everything on the push stack
 	ret
