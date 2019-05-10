4d324777be0de27f73e4f85d0b09dfdf05c44255
Arash Fotouhi <arash@arashfotouhi.com>
2019-05-09 21:26:51 -0700

    Adding interrupt test to the main kernel.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,8 +1,15 @@
 #include "../drivers/screen.h"
+#include "../cpu/isr.h"
 
 void main (void) 
 {
 	clear_screen();
 	print("MiniOS kernel successfully loaded.\n");
-	print("Welcome!");
+	print("Welcome!\n");
+
+	isr_install();
+	
+	// Testing interrupts
+	__asm__ __volatile__("int $2");
+	__asm__ __volatile__("int $3");
 }
