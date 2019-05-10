126b26f404c19f6d0607128c94c0ce0ac17edf22
Arash Fotouhi <arash@arashfotouhi.com>
2019-05-09 21:26:10 -0700

    Fixing missing register in the registers struct.

--- cpu/isr.h
+++ cpu/isr.h
@@ -38,7 +38,7 @@ extern void isr31();
 
 typedef struct {
 	u32 ds;
-	u32 edi, esi, esb, esp, edx, ecx, eax;
+	u32 edi, esi, ebp, esb, ebx, edx, ecx, eax;
 	u32 int_no, err_code;
 	u32 eip, cs, eflags, useresp, ss;
 } registers_t;
