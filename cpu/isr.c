514e569cbc47513e713277d16ccea4bd41434e1d
Arash Fotouhi <arash@arashfotouhi.com>
2019-05-10 21:57:52 -0700

    Adding IRQs

--- cpu/isr.c
+++ cpu/isr.c
@@ -2,6 +2,9 @@
 #include "idt.h"
 #include "../drivers/screen.h"
 #include "../kernel/util.h"
+#include "../kernel/low_level.h"
+
+isr_t interrupt_handler[256];
 
 void isr_install (void) {
 	set_idt_gate(0, (u32)isr0);
@@ -36,6 +39,34 @@ void isr_install (void) {
 	set_idt_gate(29, (u32)isr29);
 	set_idt_gate(30, (u32)isr30);
 	set_idt_gate(31, (u32)isr31);
+
+	port_byte_out(0x20, 0x11);
+	port_byte_out(0xA0, 0x11);
+	port_byte_out(0x21, 0x20);
+	port_byte_out(0xA1, 0x28);
+	port_byte_out(0x21, 0x04);
+	port_byte_out(0xA1, 0x02);
+	port_byte_out(0x21, 0x01);
+	port_byte_out(0xA1, 0x01);
+	port_byte_out(0x21, 0x00);
+	port_byte_out(0xA1, 0x00);
+
+	set_idt_gate(32, (u32)irq0);
+	set_idt_gate(33, (u32)irq1);
+	set_idt_gate(34, (u32)irq2);
+	set_idt_gate(35, (u32)irq3);
+	set_idt_gate(36, (u32)irq4);
+	set_idt_gate(37, (u32)irq5);
+	set_idt_gate(38, (u32)irq6);
+	set_idt_gate(39, (u32)irq7);
+	set_idt_gate(40, (u32)irq8);
+	set_idt_gate(41, (u32)irq9);
+	set_idt_gate(42, (u32)irq10);
+	set_idt_gate(43, (u32)irq11);
+	set_idt_gate(44, (u32)irq12);
+	set_idt_gate(45, (u32)irq13);
+	set_idt_gate(46, (u32)irq14);
+	set_idt_gate(47, (u32)irq15);
 
 	set_idt();
 }
@@ -84,4 +115,21 @@ void isr_handler(registers_t r) {
 	print("\n");
 	print(exception_message[r.int_no]);
 	print("\n");
+}
+
+void register_interrupt_handler(u8 n, isr_t handler)
+{
+	interrupt_handler[n] = handler;
+}
+
+void irq_handler(registers_t r)
+{
+	if (r.int_no >= 40) 
+		port_byte_out(0xA0, 0x20);
+	port_byte_out(0x20, 0x20);
+
+	if (interrupt_handler[r.int_no] != 0) {
+		isr_t handler = interrupt_handler[r.int_no];
+		handler(r);
+	}
 }
