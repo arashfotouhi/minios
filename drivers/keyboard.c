f02caad14374db8586429de5518b42f753f494b3
Arash Fotouhi <arash@arashfotouhi.com>
2019-05-11 11:43:47 -0700

    Reading from port to let the interrupt fire again later.

--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -5,7 +5,14 @@
 
 static void keyboard_callback (registers_t regs)
 {
-	print("Keyboard key pressed.\n");
+	// you have to actually read from 0x60
+	// otherwise the interrupt will only fire
+	// once and won't fire again for subsequent
+	// key presses.
+	u8 scancode = port_byte_in(0x60);
+
+	print_uint(scancode);
+	print("\n");
 }
 
 void init_keyboard (void)
