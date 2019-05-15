85635950e92717bd4c7ff14a43b215ca76ffd334
Arash Fotouhi <arash@arashfotouhi.com>
2019-05-14 21:38:01 -0700

    Adding some more helper string functions.

--- lib/libc/string.c
+++ lib/libc/string.c
@@ -12,3 +12,34 @@ void int_to_ascii (int n, char str[])
 	if(sign < 0) str[i++] = '-';
 	str[i] = '\0';
 }
+
+int strlen (char str[])
+{
+	int count = 0;
+	while(str[count] != '\0') {
+		count++;
+	}
+	return count;
+}
+
+void backspace (char str[])
+{
+	int len = strlen(str);
+	str[len-1] = '\0';
+}
+
+void append (char str[], char letter)
+{
+	int len = strlen(str);
+	str[len] = letter;
+	str[len+1] = '\0';
+}
+
+int strcmp (char s1[], char s2[])
+{
+	int i;
+	for (i=0; s1[i] == s2[i]; i++)
+		if (s1[i] == '\0')
+			return 0;
+	return s1[i] - s2[i];
+}
--- lib/libc/string.h
+++ lib/libc/string.h
@@ -2,5 +2,6 @@
 #define STRINGS_H
 
 void int_to_ascii (int n, char str[]); 
+int strcmp (char s1[], char s2[]);
 
 #endif
